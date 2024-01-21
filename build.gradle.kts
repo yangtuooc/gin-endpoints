b512655b9839eeaf8034f38bb025549d24d811a8
mawenhao <mawh@bxlsj.com>
2024-01-21 18:55:44 +0800

    chore: remove redundant config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,7 @@
-import com.strumenta.antlrkotlin.gradle.AntlrKotlinTask
-import org.jetbrains.kotlin.gradle.dsl.KotlinCompile
-
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.9.0"
     id("org.jetbrains.intellij") version "1.15.0"
-    id("com.strumenta.antlr-kotlin") version "0.1.0-RC14"
 }
 
 group = "cn.yangtuooc"
@@ -40,34 +36,11 @@ idea {
     }
 }
 
-val generateKotlinGrammarSource = tasks.register<AntlrKotlinTask>("generateKotlinGrammarSource") {
-    dependsOn("cleanGenerateKotlinGrammarSource")
 
-    // ANTLR .g4 files are under {example-project}/antlr
-    // Only include *.g4 files. This allows tools (e.g., IDE plugins)
-    // to generate temporary files inside the base path
-    source = fileTree(layout.projectDirectory.dir("src/main/kotlin/cn/yangtuooc/swag")) {
-        include("**/*.g4")
-    }
-
-    // We want the generated source files to have this package name
-    val pkgName = "cn.yangtuooc.swag"
-    packageName = pkgName
-
-    // We want visitors alongside listeners.
-    // The Kotlin target language is implicit, as is the file encoding (UTF-8)
-    arguments = listOf("-visitor")
-
-    // Generated files are outputted inside build/generatedAntlr/{package-name}
-    val outDir = "generatedAntlr/${pkgName.replace(".", "/")}"
-    outputDirectory = layout.buildDirectory.dir(outDir).get().asFile
-}
 
 
 tasks {
-    withType<KotlinCompile<*>> {
-        dependsOn(generateKotlinGrammarSource)
-    }
+
 
     // Set the JVM compatibility versions
     withType<JavaCompile> {
